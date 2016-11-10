37d0eb8f2f34182c07ac333be6bd31b18cbe05ee
virginiachae <chae.virginia@gmail.com>
2016-11-10 10:18:43 -0800

    completed sprint one

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 //require express in our app
 var express = require('express'),
   bodyParser = require('body-parser');
-
+var db = require('./models')
 // generate a new express app and call it 'app'
 var app = express();
 
@@ -25,29 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
+
 
 
 
@@ -58,7 +36,13 @@ var books = [
 ////////////////////
 //  ROUTES
 ///////////////////
-
+app.get('/api/books', function (req, res) {
+  // send all books as JSON response
+  db.Book.find(function(err, books){
+    if (err) { return console.log("index error: " + err); }
+    res.json(books);
+  });
+});
 
 
 
