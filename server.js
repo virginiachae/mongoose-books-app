2cc88a4d13a8e08ebdb9f63abdb4df925c04dacb
virginiachae <chae.virginia@gmail.com>
2016-11-10 10:45:05 -0800

    finished sprint 2

--- server.js
+++ server.js
@@ -23,10 +23,13 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  db.Book.find(function(err, books){
-    if (err) { return console.log("index error: " + err); }
-    res.json(books);
-  });
+  db.Book.find()
+    // populate fills in the author id with all the author data
+    .populate('author')
+    .exec(function(err, books){
+      if (err) { return console.log("index error: " + err); }
+      res.json(books);
+    });
 });
 
 // get one book
@@ -41,14 +44,11 @@ app.get('/api/books/:id', function (req, res) {
 // create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  var newBook = new db.Book(req.body);
-  // add newBook to database
-  newBook.save(function(err, book){
-    if (err) { return console.log("create error: " + err); }
-    console.log("created ", book.title);
-    res.json(book);
+  var newBook = new db.Book({
+    title: req.body.title,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate,
   });
-});
 
 
 // delete book
@@ -60,6 +60,20 @@ app.delete('/api/books/:id', function (req, res) {
   db.Book.findOneAndRemove({ _id: bookId }, function (err, deletedBook) {
     res.json(deletedBook);
   });
+});
+
+db.Author.findOne({name: req.body.author}, function(err, author){
+  newBook.author = author;
+  // add newBook to database
+  newBook.save(function(err, book){
+    if (err) {
+      return console.log("create error: " + err);
+    }
+    console.log("created ", book.title);
+    res.json(book);
+  });
+});
+
 });
 
 app.listen(process.env.PORT || 3000, function () {
